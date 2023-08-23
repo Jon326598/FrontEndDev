fea16fbc01c9e6e8b348b5f069abee7c541b081f
Jonathan Carvalho <jjc_oliveira@hotmail.comgit config --global user.email jjc_oliveira@hotmail.com>
2023-08-23 20:15:53 -0300

    criando nova pagina html. introduzindo primeiros comandos JS

---
+++ js/login.js
@@ -0,0 +1,20 @@
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+let btnentrar = document.getElementById('btn-entrar');
+
+const emailbanco = 'admin@admin.com'
+const senhabanco = '1234'
+
+btnentrar.addEventListener('click', () =>{
+    // alert('Fui clicado pelo javascript')
+
+    // 1º pegar o email digitado
+    // 2º pegar a senha digitada
+    // 3º validar email e senha digitados
+    // 4º se incorreto retornar mensagem de senha ou usuário incorreto
+    // 5º se estiver coreto liberar acersso a página de cadastro
+
+    window.open('cadastro-membro.html', '_self')
+
+
+})
