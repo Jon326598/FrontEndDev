92a31fe0e70095eeb38000236b23678083377980
Jonathan Carvalho <jjc_oliveira@hotmail.comgit config --global user.email jjc_oliveira@hotmail.com>
2023-09-06 22:24:26 -0300

    :sparkles: Implementando ações dos botões e ajustando o modal.

--- js/membro.js
+++ js/membro.js
@@ -1,5 +1,27 @@
 const URL = 'http://localhost:3400/clientes';
+let modoEdicao = false;
+
+let btnAdicionar = document.getElementById('btn-adicionar');
 let tabelaMembro = document.querySelector('table>tbody');
+let modalMembro = new bootstrap.Modal(document.getElementById("modal-membro"), {});
+let tituloModal = document.querySelector('h4.modal-title');
+
+let btnSalvar = document.getElementById('btn-salvar');
+let btnCancelar = document.getElementById('btn-cancelar');
+
+btnAdicionar.addEventListener('click', () => {
+    modoEdicao = false;
+    tituloModal.textContent = 'Novo membro'
+    modalMembro.show();
+});
+
+btnSalvar.addEventListener('click', () => {
+    modalMembro.hide();
+});
+
+btnCancelar.addEventListener('click', () => {
+    modalMembro.hide();
+});
 
 
 function obterMembro(){
@@ -16,11 +38,25 @@ function obterMembro(){
 
 }
 
+function editarMembro(id){
+    modoEdicao = true;
+    tituloModal.textContent = 'Editar membro'
+    modalMembro.show();
+
+    // alert('Aqui vou editar o membro ' + id);
+}
+
+function excluirMembro(id){
+    alert('Aqui vou excluir o membro ' + id);
+}
+
+
 function criarLinhaNaTabela(membro){
 
      // 1 criar linha
      let tr = document.createElement('tr');
      // 2 criar TDs
+     let tdId = document.createElement('td');
      let tdNome = document.createElement('td');
      let tdSexo = document.createElement('td');
      let tdTel = document.createElement('td');
@@ -29,6 +65,7 @@ function criarLinhaNaTabela(membro){
      let tdNasc = document.createElement('td');
      let tdAcoes = document.createElement('td');
      // 3 atualizar as informações das aTDs
+     tdId.textContent = membro.id;
      tdNome.textContent = membro.nome;
      tdSexo.textContent = membro.sexo;
      tdTel.textContent = membro.telefone;
@@ -36,9 +73,15 @@ function criarLinhaNaTabela(membro){
      tdEmail.textContent = membro.email;
      tdNasc.textContent = membro.datanasc;
 
-     tdAcoes.innerHTML =  '<button class="btn btn-primary btn-sm mr-3">Editar</button><button class="btn btn-outline-primary btn-sm mr-3">Excluir</button>'
+     tdAcoes.innerHTML =  `<button onclick="editarMembro(${membro.id})" class="btn btn-primary btn-sm mr-3">
+                                Editar
+                            </button>
+                            <button onclick="excluirMembro(${membro.id})" class="btn btn-outline-primary btn-sm mr-3">
+                                Excluir
+                            </button>`;
 
      // 4 adicionar as Tds dentro das linhas
+     tr.appendChild(tdId);
      tr.appendChild(tdNome);
      tr.appendChild(tdSexo);
      tr.appendChild(tdTel);
